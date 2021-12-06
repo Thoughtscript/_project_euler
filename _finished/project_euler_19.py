1358e543a0ae8d1217d7b609f715618afe7b4c35
Thoughtscript <adam.intae.gerard@protonmail.com>
2021-12-06 15:47:51 -0600

    #19

--- _finished/project_euler_19.py
+++ _finished/project_euler_19.py
@@ -1,5 +1,7 @@
 # https://projecteuler.net/problem=19
 
+import datetime
+
 if __name__ == '__main__':
 
     try:
@@ -19,13 +21,23 @@ if __name__ == '__main__':
         #print(is_leap_year(1900))
         #print(is_leap_year(2200))
 
-        def solve():
-            # Iterate Months
-            # Check if 1st of Month is a Sunday
+        # Iterate Months
+        # Check if 1st of Month is a Sunday
 
-            # Iterate Years
-            # Between 1/1/1901 to 12/31/2000
-            return
+        # Iterate Years
+        # Between 1/1/1901 to 12/31/2000
+
+        def solve():
+            count = 0
+            for year in range(1901,2001,1):
+                for month in range(1,13,1):
+                    d = datetime.datetime(year, month, 1)
+                    if d.strftime('%A') == 'Sunday':
+                        count = count + 1
+                        print("Found: " + str(d) + " is a " + d.strftime('%A'))
+            
+            print(count)
+            return count
 
         solve()
 
