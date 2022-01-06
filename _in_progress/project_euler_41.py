598099b1c6c70d0e0cf8040f9ed1939ed5b7c558
Thoughtscript <adam.intae.gerard@protonmail.com>
2022-01-05 20:24:46 -0600

    finishing #41

--- _in_progress/project_euler_41.py
+++ _in_progress/project_euler_41.py
@@ -1,6 +1,6 @@
 # https://projecteuler.net/problem=41
 
-# This is equivalent to three mediums on LeetCode:
+# This is equivalent to three+ mediums on LeetCode:
 # https://leetcode.com/problems/permutations/ - Problem #46 Medium
 # https://leetcode.com/problems/combinations/ - Problem #77 Medium
 # https://leetcode.com/problems/count-primes/ - Problem #204 Medium
@@ -11,7 +11,11 @@ if __name__ == '__main__':
 
         LOW_PRIMES = [2,3,5,7,11,13,17]
 
+        # This is Greedily coded to find prime numbers above 17.
+        # E.g. - it won'y return 17 as prime!
         def find_primes(pandigital_numbers):
+            primes = []
+
             for x in range(0, len(pandigital_numbers), 1):
                 num = pandigital_numbers[x]
                 divisors = []
@@ -23,17 +27,40 @@ if __name__ == '__main__':
                         divisors.append(prime)
                         num = num / prime
 
+                # Either a number is Prime
+                # Or it has 2 or more Prime Factors
+                # So, if this is > 1, it's not Prime
+                if len(divisors) > 1:
+                    # print(divisors)
+                    continue
+
+                not_prime = False
+
                 for y in range(18, num, 1):
                     if num % y == 0:
                         divisors.append(y)
                         num = num / y
 
+                    if len(divisors) > 1:
+                        not_prime = True
+                        break
+                
+                if not_prime:
+                    continue
+
                 divisors.append(num)
-                print(divisors)
+                divisors.append(1)
+                
+                if len(divisors) == 2:
+                    primes.append(num)
+
+            print(" ========= Primes found: ========= ")
+            print(primes)
+            return primes
 
         # ----------------- #
 
-        def deep_copy(arr):
+        def make_key(arr):
             string_result = ""
 
             for x in range(0, len(arr), 1):
@@ -42,6 +69,7 @@ if __name__ == '__main__':
             return string_result
 
         def permute(original_arr):
+            # print(original_arr)
             transfer_arr = original_arr
             heaps_results = {}
 
@@ -52,7 +80,7 @@ if __name__ == '__main__':
 
             def heaps_algorithm(n):
                 if n == 1: 
-                    key = deep_copy(transfer_arr)
+                    key = make_key(transfer_arr)
                     heaps_results[key] = int(key)
             
                 for i in range(0, n, 1):
@@ -63,7 +91,7 @@ if __name__ == '__main__':
                     swap(n-1, v)
 
             heaps_algorithm(len(original_arr))
-            print(heaps_results)
+            # print(heaps_results)
             return heaps_results
 
         # permute([0])
@@ -77,7 +105,7 @@ if __name__ == '__main__':
 
         # 1 2 3
         # 1 2 4
-        # 1 3 4
+        # 1 3 4c
 
         # 1 2 3 4
         # 1 2 3 5
@@ -85,48 +113,71 @@ if __name__ == '__main__':
         # 1 3 4 5
         # 2 3 4 5
 
-        def combination(arr, sz):
-            LEN = len(arr)
+        def deep_copy(arr):
+            new_arr = []
 
-            if sz > LEN:
-                return 
+            for x in range(0, len(arr), 1):
+                new_arr.append(arr[x])
 
-            if sz == LEN:
-                print(arr)
-                return arr
+            return new_arr
 
-            results = []
+        def combination(n, k):
+            ORIG_K = k
+            result = []
+            temp = []
 
-            for x in range(0, LEN, 1):
-                combo = [arr[x]]
-                
-                for y in range(x + 1, x + sz, 1):
-                    if y >= LEN:
-                        break
+            def inner(n, l, k):
+                if len(temp) == ORIG_K:
+                    new_arr = deep_copy(temp)
+                    # print(new_arr)
+                    result.append(new_arr)
 
-                    combo.append(arr[y])
+                for x in range(l, n + 1, 1):
+                    temp.append(x)
+                    inner(n, x + 1, k - 1)
 
-                results.append(combo)
+                    # Modified from:
+                    # https://www.geeksforgeeks.org/make-combinations-size-k/?ref=gcse
+                    # Super helpful!
+                    temp.pop()
 
-            print(results)
-            return results
+            inner(n, 0, k)
+            print(" ========= Combinations: ========= ")
+            print(result)
+            return result
 
-        DIGITS = [1,2,3,4,5]
-        # combination(DIGITS, 4)
+        # combination(4, 4)
+        # combination(3, 3)
+        # combination(8, 3)
+        # combination(5, 5)
+        # combination(9, 5)
+
+        # ----------------- #
 
         # The largest such n-digit Pandigital Number will be less than or equal to 9876543210.
         # I solved problem 43 previously:
-        # 1406357289, 1430952867, 1460357289, 4106357289, 4130952867, 4160357289]
+        # 1406357289, 1430952867, 1460357289, 4106357289, 4130952867, 4160357289
         # are the only 6 Pandigital Numbers of length 10.
         # None of those are prime numbers, so the largest one is less than length 10.
 
-        def solve():
-            for x in range(2, 10, 1):
-                combination = combination(DIGITS, x)
-                heaps = permute(combination)
-                print(find_primes(heaps))
+        def map_to_arr(hm):
+            result = []
+            hmv = hm.values()
 
-        # solve()
+            for x in range(0, len(hmv), 1):
+                result.append(int(hmv[x]))
+
+            return result
+
+        def solve():
+            # Can modify this row
+            for x in range(2, 10, 1):
+                combos = combination(9, x)
+                for y in range(0, len(combos), 1):
+                    heaps = permute(combos[y])
+                    find_primes(map_to_arr(heaps))
+
+        solve()
 
     except Exception as ex:
 
