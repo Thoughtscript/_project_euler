d5f6f90312da123e7d44207c21fdec5fcc4a4d2c
Adam Gerard <adam.intae.gerard@protonmail.com>
2022-08-30 13:30:55 -0500

    improve #345

--- _in_progress/project_euler_345.py
+++ _in_progress/project_euler_345.py
@@ -1,4 +1,5 @@
 # https://projecteuler.net/problem=345
+import copy
 
 if __name__ == '__main__':
 
@@ -22,17 +23,150 @@ if __name__ == '__main__':
             [813, 883, 451, 509, 615, 77, 281, 613, 459, 205, 380, 274, 302, 35, 805]
         ]
 
-        mx = 0
         ROWS = len(matrix)
         COLS = len(matrix[0])
 
-        
-        row_counter = []
-        row_counter = [0 for r in ROWS]
-        cols_counter = []
-        cols_counter = [0 for c in COLS]
+        def make_cols():
+            cols = []
 
-        print(sum)
+            for c in range(0, len(matrix), 1):
+                col = []
+
+                for r in range(0, len(matrix), 1):
+                    col.append(matrix[r][c])
+
+                cols.append(col)
+                # print(col)
+
+            # print(cols)
+            return cols
+
+        def generate_perms():
+            perms = []
+            # fh = open("permutations.txt", "a")
+
+            for a in range(0, ROWS, 1):
+                tempa = {}
+                tempa[a] = 1
+
+                for b in range(0, ROWS, 1):
+                    tempb = copy.deepcopy(tempa)
+                    if not tempb.get(b) == None:
+                        continue
+                    tempb[b] = 1
+
+                    for c in range(0, ROWS, 1):
+                        tempc = copy.deepcopy(tempb)
+                        if not tempc.get(c) == None:
+                            continue
+                        tempc[c] = 1
+
+                        for d in range(0, ROWS, 1):
+                            tempd = copy.deepcopy(tempc)
+                            if not tempd.get(d) == None:
+                                continue
+                            tempd[d] = 1
+
+                            for e in range(0, ROWS, 1):
+                                tempe = copy.deepcopy(tempd)
+                                if not tempe.get(e) == None:
+                                    continue
+                                tempe[e] = 1
+
+                                for f in range(0, ROWS, 1):
+                                    tempf = copy.deepcopy(tempe)
+                                    if not tempf.get(f) == None:
+                                        continue
+                                    tempf[f] = 1
+
+                                    for g in range(0, ROWS, 1):
+                                        tempg = copy.deepcopy(tempf)
+                                        if not tempg.get(g) == None:
+                                            continue
+                                        tempg[g] = 1
+
+                                        for h in range(0, ROWS, 1):
+                                            temph = copy.deepcopy(tempg)
+                                            if not temph.get(h) == None:
+                                                continue
+                                            temph[h] = 1
+
+                                            for i in range(0, ROWS, 1):
+                                                tempi = copy.deepcopy(temph)
+                                                if not tempi.get(i) == None:
+                                                    continue
+                                                tempi[i] = 1
+
+                                                for j in range(0, ROWS, 1):
+                                                    tempj = copy.deepcopy(tempi)
+                                                    if not tempj.get(j) == None:
+                                                        continue
+                                                    tempj[j] = 1
+
+                                                    for k in range(0, ROWS, 1):
+                                                        tempk = copy.deepcopy(tempj)
+                                                        if not tempk.get(k) == None:
+                                                            continue
+                                                        tempk[k] = 1
+
+                                                        for l in range(0, ROWS, 1):
+                                                            templ = copy.deepcopy(tempk)
+                                                            if not templ.get(l) == None:
+                                                                continue
+                                                            templ[l] = 1
+
+                                                            for m in range(0, ROWS, 1):
+                                                                tempm = copy.deepcopy(templ)
+                                                                if not tempm.get(m) == None:
+                                                                    continue
+                                                                tempm[m] = 1
+
+                                                                for n in range(0, ROWS, 1):
+                                                                    tempn = copy.deepcopy(tempm)
+                                                                    if not tempn.get(n) == None:
+                                                                        continue
+                                                                    tempn[n] = 1
+
+                                                                    for o in range(0, ROWS, 1):
+                                                                        tempo = copy.deepcopy(tempn)
+                                                                        if not tempo.get(o) == None:
+                                                                            continue
+
+                                                                        # print([a,b,c,d,e,f,g,h,i,j,k,l,m,n,o])
+                                                                        # fh.write(str([a,b,c,d,e,f,g,h,i,j,k,l,m,n,o]) + "\n")
+                                                                        perms.append([a,b,c,d,e,f,g,h,i,j,k,l,m,n,o])
+                                                                        
+
+            # fh.close()
+            # print(perms)
+            return perms
+
+        def solve():
+            cols = make_cols()
+            perms = generate_perms()
+            mx = 0
+
+            for x in range(0, len(perms), 1):
+                sum = 0
+
+                for y in range(0, len(perms[x]), 1):
+                    sum += cols[y][perms[x][y]]
+
+                if (sum < mx):
+                    print("New max found: " + str(sum))
+                    print("Matrix sum " + str(perms[x]))
+                    mx = sum
+
+            print(mx)
+            return mx
+
+        solve()
+
+        # Three ways:
+
+        # i. Factorial brute force - permutations - too slow.
+        # ii. (Pre-)sort each row and then col by sorted rows.
+        # iii. Solve upper-right to lower-left expanding the matrix sum from n to m (n < m)?
 
     except Exception as ex:
 
