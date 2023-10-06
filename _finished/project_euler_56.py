7900aef2b4ea2f530d106bb23ef31b04d464c15c
Adam Gerard <adam.intae.gerard@protonmail.com>
2023-10-06 16:32:28 -0500

    #56

---
+++ _finished/project_euler_56.py
@@ -0,0 +1,31 @@
+# https://projecteuler.net/problem=56
+
+if __name__ == '__main__':
+
+    try:
+
+        def digit_sum(num_str):
+            sum = 0
+
+            for x in range(0, len(num_str), 1):
+                sum += int(num_str[x])
+
+            return sum
+
+        def solve():
+            mx_num = 0
+
+            for a in range(0, 100, 1):
+                for b in range(0, 100, 1):
+                    num = pow(a, b)
+                    num_str = str(num)
+                    digit_sum_score =  digit_sum(num_str)
+                    if mx_num < digit_sum_score:
+                        mx_num = max(mx_num, digit_sum_score)
+                        print("New largest digit sum found: " + str(digit_sum_score) + " for num " + num_str)
+            
+        solve() # New largest digit sum found: 972 for num 3848960788934848611927795802824596789608451156087366034658627953530148126008534258032267383768627487094610968554286692697374726725853195657679460590239636893953692985541958490801973870359499
+
+    except Exception as ex:
+
+        print('Exception: ' + str(ex))
